65e1a4caba365ee12d880138bcb8e2f3205039e2
LeeGwangSik <jine9220@naver.com>
2025-05-28 14:44:19 +0900

    로그인 세션 추가

--- src/main/java/com/airplane/plane/HomeController.java
+++ src/main/java/com/airplane/plane/HomeController.java
@@ -6,12 +6,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.airplane.user.LoginRequestCommand;
 import com.example.airport.AirinfoDto;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -76,10 +80,14 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "reserveSeat", method = RequestMethod.POST)
-	public String selectSeat()
-	{
+	public String selectSeat(HttpSession session) {
 		//업데이트
-		return "redirect:/";
+		if(session.getAttribute("loginUser")==null) {
+			return "redirect:/login";
+		}else {
+			return "redirect:/";
+		}
+		
 	}
 	
 
